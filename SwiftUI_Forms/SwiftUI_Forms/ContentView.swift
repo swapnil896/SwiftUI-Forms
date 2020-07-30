35ef455fd723e83c141cf36491cb53750fcf3183
Swapnil Magar <magarswapnil896@gmail.com>
2020-07-30 22:24:40 +0530

    Intial code base

---
+++ SwiftUI_Forms/SwiftUI_Forms/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Forms
+//
+//  Created by Swapnil  Magar on 30/07/20.
+//  Copyright © 2020 Swapnil Magar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
